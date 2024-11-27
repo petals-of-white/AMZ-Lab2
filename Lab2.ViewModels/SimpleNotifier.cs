e9b1ffa8fc18426dd047a4afab0800a6613e2f6d
Maksym Syvash <maksym_syvash@outlook.com>
2024-11-27 21:37:54 +0200

    Added viewmodels and views empty projects

---
+++ Lab2.ViewModels/SimpleNotifier.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Lab2.ViewModels;
+
+public class SimpleNotifier : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
+
